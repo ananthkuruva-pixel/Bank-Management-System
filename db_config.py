4d9bfd385b40778713d3d0501dd87d537953042b
ananthkuruva-pixel <ananthkuruva@gmail.com>
2026-02-16 19:47:26 +0530

    Delete db_config.py

--- db_config.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector as myconn
-
-def connect():
-    return myconn.connect(
-        host="localhost",
-        user="root",
-        password="2343",
-        database="bank_system"
-    )
